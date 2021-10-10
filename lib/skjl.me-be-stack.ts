23adee62b4cd9de328a046b10285cc374a091a8b
mim <mim3dot@gmail.com>
2021-10-10 14:27:30 -0500

    Adding some infrastructure including a DynamoDB table, next I'll add lambdas and Gateways, etc

--- lib/skjl.me-be-stack.ts
+++ lib/skjl.me-be-stack.ts
@@ -1,18 +1,23 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as ddb from '@aws-cdk/aws-dynamodb';
 
 export class SkjlMeBeStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'SkjlMeBeQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
+        const dynamoDbSkjlPubs = new ddb.Table(this, 'SklMePubsTable', {
+            partitionKey: {name: 'pubKey', type: ddb.AttributeType.STRING},
+            sortKey: {name: 'calendarId', type: ddb.AttributeType.STRING},
+            tableName: 'SkjlMePubs',
+            removalPolicy: cdk.RemovalPolicy.RETAIN,
+            billingMode: ddb.BillingMode.PAY_PER_REQUEST,
+        })
 
-    const topic = new sns.Topic(this, 'SkjlMeBeTopic');
+        dynamoDbSkjlPubs.addGlobalSecondaryIndex({
+            indexName: 'calendarIndex',
+            partitionKey: {name: 'calendarId', type: ddb.AttributeType.STRING}
+        })
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
-  }
+
+    }
 }
